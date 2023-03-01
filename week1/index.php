fea0a81620982e631a9e7ace48f59fe8f6c39e73
icav1616 <126706510+icav1616@users.noreply.github.com>
2023-03-02 00:43:17 +0700

    Add files via upload

---
+++ week1/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $nama = 'icha';
+    $umur = 18;
+    $berat = 53;
+
+    echo 'Nama saya : '  . $nama;
+    echo '<br>Umur saya : '  . $umur;
+    echo '<br>Berat saya : '  . $berat;
+
